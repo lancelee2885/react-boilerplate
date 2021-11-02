bb4273b6f0f70452470b09e858ea0c2d9cace35f
lancelee2885 <lancelee2885@gmail.com>
2021-11-01 22:23:11 -0700

    basic actions added

--- app/containers/App/action.js
+++ app/containers/App/action.js
@@ -22,7 +22,7 @@ import { LOAD_ITEMS, LOAD_ITEMS_ERROR, LOAD_ITEMS_SUCCESS } from './constants';
  *
  * @return {object} An action object with a type of LOAD_REPOS
  */
-export function loadRepos() {
+export function loadItems() {
   return {
     type: LOAD_ITEMS,
   };
@@ -36,11 +36,10 @@ export function loadRepos() {
  *
  * @return {object}      An action object with a type of LOAD_REPOS_SUCCESS passing the repos
  */
-export function reposLoaded(repos, username) {
+export function itemsLoaded(items) {
   return {
     type: LOAD_ITEMS_SUCCESS,
-    repos,
-    username,
+    items,
   };
 }
 
@@ -51,7 +50,7 @@ export function reposLoaded(repos, username) {
  *
  * @return {object}       An action object with a type of LOAD_REPOS_ERROR passing the error
  */
-export function repoLoadingError(error) {
+export function itemsLoadingError(error) {
   return {
     type: LOAD_ITEMS_ERROR,
     error,
