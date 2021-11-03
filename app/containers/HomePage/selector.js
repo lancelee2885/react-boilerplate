0d86a327bad82ab21b8eb990ac04adc118cd6f5b
lancelee2885 <lancelee2885@gmail.com>
2021-11-03 00:54:02 -0700

    homepage selectors created

--- app/containers/HomePage/selector.js
+++ app/containers/HomePage/selector.js
@@ -1,16 +1,37 @@
-/**
- * Homepage selectors
- */
-
 import { createSelector } from 'reselect';
 import { initialState } from './reducer';
 
-const selectHome = state => state.home || initialState;
+/**
+ * Direct selector to the mainPage state domain
+ */
 
-const makeSelectUsername = () =>
+const selectHomePageDomain = state => state.homePage || initialState;
+
+/**
+ * Other specific selectors
+ */
+
+/**
+ * Default selector used by HomePage
+ */
+
+const makeSelectHomePage = () =>
   createSelector(
-    selectHome,
-    homeState => homeState.items,
+    selectHomePageDomain,
+    substate => substate,
   );
 
-export { selectHome, makeSelectUsername };
+const makeCryptosSelector = () =>
+  createSelector(
+    selectHomePageDomain,
+    substate => substate.cryptos,
+  );
+
+const makeLoadingSelector = () =>
+  createSelector(
+    selectHomePageDomain,
+    substate => substate.loading,
+  );
+
+export default makeSelectHomePage;
+export { makeSelectHomePage, makeCryptosSelector, makeLoadingSelector };
