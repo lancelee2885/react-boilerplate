5dbee34966dd8da135188fe6de1fb81bf564ed05
lancelee2885 <lancelee2885@gmail.com>
2021-11-03 02:04:04 -0700

    adding navbar

--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -12,14 +12,16 @@ import { Switch, Route } from 'react-router-dom';
 
 import HomePage from 'containers/HomePage/Loadable';
 import NotFoundPage from 'containers/NotFoundPage/Loadable';
+import Navbar from '../../components/Navbar';
 
 import GlobalStyle from '../../global-styles';
 
 export default function App() {
   return (
     <div>
+      <Navbar />
       <Switch>
-        <Route exact path="/" component={HomePage} />
+        <Route exact path="/cryptos" component={HomePage} />
         <Route component={NotFoundPage} />
       </Switch>
       <GlobalStyle />
