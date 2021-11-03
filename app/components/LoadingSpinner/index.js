642630a2564d9d2388caec353aaa459f1befb421
lancelee2885 <lancelee2885@gmail.com>
2021-11-03 03:15:40 -0700

    loading spinner styled component created

---
+++ app/components/LoadingSpinner/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Spinner from './LoadingElements';
+
+/** LoadingSpinner:
+ *  - Indication when page is loading
+ */
+function LoadingSpinner() {
+  return <Spinner />;
+}
+
+export default LoadingSpinner;
