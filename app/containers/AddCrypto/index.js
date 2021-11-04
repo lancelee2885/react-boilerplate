4157b1610fd1f28d5ac9c7b2106587ae580ca7a4
lancelee2885 <lancelee2885@gmail.com>
2021-11-03 20:17:09 -0700

    update form fields

--- app/containers/AddCrypto/index.js
+++ app/containers/AddCrypto/index.js
@@ -7,44 +7,85 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { Helmet } from 'react-helmet';
-import { FormattedMessage } from 'react-intl';
 import { createStructuredSelector } from 'reselect';
 import { compose } from 'redux';
 
 import { useInjectSaga } from 'utils/injectSaga';
 import { useInjectReducer } from 'utils/injectReducer';
-import makeSelectAddCrypto from './selectors';
+import Form from './Form';
+import Input from './Input';
+import makeSelectAddCrypto, { makeFormDataSelector } from './selectors';
 import reducer from './reducer';
 import saga from './saga';
-import messages from './messages';
+import { changeFormData } from './actions';
 
-export function AddCrypto() {
+export function AddCrypto({ formData, onChangeFormData }) {
   useInjectReducer({ key: 'addCrypto', reducer });
   useInjectSaga({ key: 'addCrypto', saga });
 
   return (
     <div>
-      <Helmet>
-        <title>AddCrypto</title>
-        <meta name="description" content="Description of AddCrypto" />
-      </Helmet>
-      <FormattedMessage {...messages.header} />
+      <Form>
+        <label htmlFor="symbol">
+          Symbol:
+          <Input
+            type="text"
+            name="symbol"
+            value={formData.symbol}
+            onChange={evt => onChangeFormData(evt, formData)}
+          />
+        </label>
+        <label htmlFor="name">
+          Name:
+          <Input
+            type="text"
+            name="name"
+            value={formData.name}
+            onChange={evt => onChangeFormData(evt, formData)}
+          />
+        </label>
+        <label htmlFor="description">
+          Description:
+          <Input
+            type="text"
+            name="description"
+            value={formData.description}
+            onChange={evt => onChangeFormData(evt, formData)}
+          />
+        </label>
+        <label htmlFor="icon">
+          Icon Link:
+          <Input
+            type="text"
+            name="icon"
+            value={formData.icon}
+            onChange={evt => onChangeFormData(evt, formData)}
+          />
+        </label>
+      </Form>
     </div>
   );
 }
 
 AddCrypto.propTypes = {
-  dispatch: PropTypes.func.isRequired,
+  onChangeFormData: PropTypes.func,
+  formData: PropTypes.object,
 };
 
 const mapStateToProps = createStructuredSelector({
   addCrypto: makeSelectAddCrypto(),
+  formData: makeFormDataSelector(),
 });
 
-function mapDispatchToProps(dispatch) {
+export function mapDispatchToProps(dispatch) {
   return {
-    dispatch,
+    onChangeFormData: (evt, formData) => {
+      const newData = {
+        ...formData,
+        [evt.target.name]: evt.target.value,
+      };
+      dispatch(changeFormData(newData));
+    },
   };
 }
 
