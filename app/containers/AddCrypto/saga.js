929bcf4edeeccdff97d35fb73071ca95c8f293a1
lancelee2885 <lancelee2885@gmail.com>
2021-11-04 16:23:24 -0700

    docstrings change

--- app/containers/AddCrypto/saga.js
+++ app/containers/AddCrypto/saga.js
@@ -1,5 +1,5 @@
 /**
- * Gets the repositories of the user from Github
+ * Submit form data to backedn server.
  */
 
 import axios from 'axios';
@@ -12,6 +12,7 @@ import { makeFormDataSelector } from './selectors';
 
 const requestURL = `http://localhost:3000/api/`;
 
+// formdata submission
 async function submitToServer(data) {
   try {
     await axios.post(requestURL, data);
@@ -21,10 +22,10 @@ async function submitToServer(data) {
 }
 
 /**
- * Github repos request/response handler
+ * update states to load new sets of cryptos
  */
 export function* createNewCrypto() {
-  // Select username from store
+  // Select formData from store
   const data = yield select(makeFormDataSelector());
 
   try {
@@ -41,9 +42,5 @@ export function* createNewCrypto() {
  * Root saga manages watcher lifecycle
  */
 export default function* loadNewData() {
-  // Watches for LOAD_REPOS actions and calls getRepos when one comes in.
-  // By using `takeLatest` only the result of the latest API call is applied.
-  // It returns task descriptor (just like fork) so we can continue execution
-  // It will be cancelled automatically on component unmount
   yield takeLatest(SUBMIT_FORM_DATA, createNewCrypto);
 }
