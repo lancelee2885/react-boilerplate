d4b93e57cca2f7b2bda6fb0d05cf84f4f7baeab7
lancelee2885 <lancelee2885@gmail.com>
2021-11-03 23:52:01 -0700

    button for submit a form

---
+++ app/components/SubmitBtn/SubmitBtnElements.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const SubmitBtnWrapper = styled.button`
+  margin-top: 32px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
---
+++ app/components/SubmitBtn/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { SubmitBtnWrapper } from './SubmitBtnElements';
+
+/** SubmitBtn:
+ *  - Simple button for sumbitting a form.
+ */
+function SubmitBtn() {
+  return <SubmitBtnWrapper> Submit </SubmitBtnWrapper>;
+}
+
+export default SubmitBtn;
