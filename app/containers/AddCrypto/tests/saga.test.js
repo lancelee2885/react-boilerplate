390eab3d687447939478d0ecd319104a9ee915a9
lancelee2885 <lancelee2885@gmail.com>
2021-11-05 02:23:25 -0700

    addPage: saga tests

--- app/containers/AddCrypto/tests/saga.test.js
+++ app/containers/AddCrypto/tests/saga.test.js
@@ -1,15 +1,53 @@
 /**
- * Test sagas
+ * Tests for AddCrypto sagas
  */
 
+import { call, put, takeLatest } from 'redux-saga/effects';
+
+import { SUBMIT_FORM_DATA } from '../constants';
+import { formDataSubmitted, formDataError } from '../actions';
+
+import loadNewData, { createNewCrypto, submitToServer } from '../saga';
+
 /* eslint-disable redux-saga/yield-effects */
-// import { take, call, put, select } from 'redux-saga/effects';
-// import addCryptoSaga from '../saga';
+describe('createNewCrypto Saga', () => {
+  let createCryptosGenerator;
 
-// const generator = addCryptoSaga();
+  // We have to test twice, once for a successful load and once for an unsuccessful one
+  // so we do all the stuff that happens beforehand automatically in the beforeEach
+  beforeEach(() => {
+    createCryptosGenerator = createNewCrypto();
 
-describe('addCryptoSaga Saga', () => {
-  it('Expect to have unit tests specified', () => {
-    expect(true).toEqual(false);
+    const callDescriptor = createCryptosGenerator.next().value;
+    expect(callDescriptor).toMatchSnapshot();
+  });
+
+  it('should dispatch the submitToServer action if it sends the data successfully', () => {
+    const callDescriptor = createCryptosGenerator.next('test');
+    expect(callDescriptor.value).toEqual(call(submitToServer, 'test'));
+  });
+
+  it('should dispatch the formDataSubmitted action if it sends the data successfully', () => {
+    createCryptosGenerator.next('test');
+    const putDescriptor = createCryptosGenerator.next('test');
+    expect(putDescriptor.value).toEqual(put(formDataSubmitted('test')));
+  });
+
+  it('should call the formDataError action if the response errors', () => {
+    const response = new Error('Some error');
+    createCryptosGenerator.next();
+    const putDescriptor = createCryptosGenerator.throw(response).value;
+    expect(putDescriptor).toEqual(put(formDataError(response)));
+  });
+});
+
+describe('loadNewData Saga', () => {
+  const newDataSaga = loadNewData();
+
+  it('should start task to watch for LOAD_REPOS action', () => {
+    const takeLatestDescriptor = newDataSaga.next().value;
+    expect(takeLatestDescriptor).toEqual(
+      takeLatest(SUBMIT_FORM_DATA, createNewCrypto),
+    );
   });
 });
