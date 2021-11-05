0e2ea3f46c30c7b98404f4987c9c2a0853f71568
lancelee2885 <lancelee2885@gmail.com>
2021-11-05 02:22:58 -0700

    addPage: actions tests

--- app/containers/AddCrypto/tests/actions.test.js
+++ app/containers/AddCrypto/tests/actions.test.js
@@ -1,13 +1,61 @@
-import { defaultAction } from '../actions';
-import { DEFAULT_ACTION } from '../constants';
+import {
+  CHANGE_FORM_DATA,
+  SUBMIT_FORM_DATA,
+  SUBMIT_FORM_DATA_SUCCESS,
+  SUBMIT_FORM_DATA_ERROR,
+} from '../constants';
 
-describe('AddCrypto actions', () => {
-  describe('Default Action', () => {
-    it('has a type of DEFAULT_ACTION', () => {
-      const expected = {
-        type: DEFAULT_ACTION,
+import {
+  changeFormData,
+  submitFormData,
+  formDataSubmitted,
+  formDataError,
+} from '../actions';
+
+describe('Adding Page Actions', () => {
+  describe('changeFormData', () => {
+    it('should return the correct type', () => {
+      const expectedResult = {
+        type: CHANGE_FORM_DATA,
       };
-      expect(defaultAction()).toEqual(expected);
+
+      expect(changeFormData()).toEqual(expectedResult);
+    });
+  });
+
+  describe('submitFormData', () => {
+    it('should return the correct type', () => {
+      const expectedResult = {
+        type: SUBMIT_FORM_DATA,
+      };
+
+      expect(submitFormData()).toEqual(expectedResult);
+    });
+  });
+
+  describe('formDataSubmitted', () => {
+    it('should return the correct type and the passed formData', () => {
+      const fixture = 'test';
+      const expectedResult = {
+        type: SUBMIT_FORM_DATA_SUCCESS,
+        formData: fixture,
+      };
+
+      expect(formDataSubmitted(fixture)).toEqual(expectedResult);
+    });
+  });
+
+  describe('SUBMIT_FORM_DATA_ERROR', () => {
+    it('should return the correct type and the error', () => {
+      const fixture = {
+        msg: 'Something went wrong!',
+      };
+      const expectedResult = {
+        type: SUBMIT_FORM_DATA_ERROR,
+        err: fixture,
+      };
+
+      expect(formDataError(fixture)).toEqual(expectedResult);
     });
   });
 });
