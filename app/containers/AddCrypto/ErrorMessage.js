13799dc25f6cc54b729bf34feea43f7b912212e6
lancelee2885 <lancelee2885@gmail.com>
2021-11-06 17:19:55 -0700

    custom error styles

---
+++ app/containers/AddCrypto/ErrorMessage.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+const ErrorMessage = styled.p`
+  text-align: center;
+  padding: 5px;
+  height: fit-content;
+  width: fit-content;
+  background-color: #d9534f;
+  border-radius: 5px;
+`;
+
+export default ErrorMessage;
